4133ba8419ea58e8f9493ba04c3739f08000e7e8
Pallavi Shrestha <pallavi@fusemachines.com>
2022-09-07 01:04:24 +0545

    test2 commit

---
+++ test2.py
@@ -0,0 +1,11 @@
+#Python input, output and import
+print('testing output data')
+
+#changing separator
+print('a', 'b','c','d')
+print('a', 'b','c','d', sep='#')
+print('a','b','c','d', sep='*', end = '%')
+
+#output formatting   str.format()
+
+

2e791d750f24820ccb936d7b1170d730bf1ccbb4
Pallavi Shrestha <pallavi@fusemachines.com>
2022-09-06 14:18:49 +0545

    python test commit

---
+++ test.py
@@ -0,0 +1,26 @@
+#python syntax
+print("Hello, World!")
+if 5>2:
+    print ("5 is greater than 2")
+
+#multiline comments
+""" This is a 
+multiline
+comment """
+
+#variables
+x= 5
+y=10
+print(x+y)
+
+y = 'John' #data type of y changed to str
+x=float(5)  #data type of x changed to float
+print(x)
+print(y)
+print(type(x))  #to get data type of variable x
+print(type(y))
+
+
+
+
+
